08e680cfa77a01c408560e385f18056c041f760d
zhouweiming <373518155@qq.com>
2019-10-18 12:23:53 +0800

    添加了消息列表框架

---
+++ twant/app/src/main/java/com/ftofs/twant/fragment/MessageListFragment.java
@@ -0,0 +1,55 @@
+package com.ftofs.twant.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ftofs.twant.R;
+import com.ftofs.twant.log.SLog;
+
+/**
+ * 消息列表Fragment
+ * @author zwm
+ */
+public class MessageListFragment extends BaseFragment implements View.OnClickListener {
+    public static MessageListFragment newInstance(int tplClass) {
+        Bundle args = new Bundle();
+        
+        MessageListFragment fragment = new MessageListFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_message_list, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_back) {
+            pop();
+        }
+    }
+
+    @Override
+    public boolean onBackPressedSupport() {
+        SLog.info("onBackPressedSupport");
+        pop();
+        return true;
+    }
+}
+
+
