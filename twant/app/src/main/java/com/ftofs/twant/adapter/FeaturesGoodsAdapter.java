b9b1db029f53cfecb3b9ca177ea47a675515c3df
zhouweiming <373518155@qq.com>
2020-03-19 12:40:42 +0800

    禪道4325 鎮店之寶功能的交互的視覺優化

--- twant/app/src/main/java/com/ftofs/twant/adapter/FeaturesGoodsAdapter.java
+++ twant/app/src/main/java/com/ftofs/twant/adapter/FeaturesGoodsAdapter.java
@@ -1,21 +1,28 @@
 package com.ftofs.twant.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.ftofs.twant.R;
 import com.ftofs.twant.constant.Constant;
 import com.ftofs.twant.entity.StoreGoodsItem;
 import com.ftofs.twant.fragment.GoodsDetailFragment;
 import com.ftofs.twant.util.StringUtil;
 import com.ftofs.twant.util.Util;
+import com.ftofs.twant.widget.HwLoadingView;
 
 import java.util.List;
 
@@ -32,6 +39,7 @@ public class FeaturesGoodsAdapter  extends RecyclerView.Adapter<FeaturesGoodsAda
         TextView tvGoodsName;
         TextView tvGoodsJingle;
         TextView tvGoodsPrice;
+        HwLoadingView loadingView;
 
         public ViewHolder(View view) {
             super(view);
@@ -40,6 +48,7 @@ public class FeaturesGoodsAdapter  extends RecyclerView.Adapter<FeaturesGoodsAda
             tvGoodsName = view.findViewById(R.id.tv_goods_name);
             tvGoodsJingle = view.findViewById(R.id.tv_goods_jingle);
             tvGoodsPrice = view.findViewById(R.id.tv_goods_price);
+            loadingView = view.findViewById(R.id.loading_view);
         }
     }
 
@@ -61,7 +70,18 @@ public class FeaturesGoodsAdapter  extends RecyclerView.Adapter<FeaturesGoodsAda
         position = position % storeGoodsItemList.size();
         StoreGoodsItem item = storeGoodsItemList.get(position);
 
-        Glide.with(context).load(StringUtil.normalizeImageUrl(item.imageSrc)).centerCrop().into(holder.goodsImage);
+        Glide.with(context).load(StringUtil.normalizeImageUrl(item.imageSrc)).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                holder.loadingView.setVisibility(View.GONE);  // 如果加載完成，隱藏loadingView
+                return false;
+            }
+        }).centerCrop().into(holder.goodsImage);
         holder.tvGoodsName.setText(item.goodsName);
         holder.tvGoodsJingle.setText(item.jingle);
         holder.tvGoodsPrice.setText(StringUtil.formatPrice(context, item.price, 1,false));
