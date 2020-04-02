e8e014be4b5eff0cbfa8a1370f3f1c68bd80a4b7
zhouweiming <373518155@qq.com>
2020-04-02 14:19:45 +0800

    Build101測試版

--- twant/app/src/main/java/com/ftofs/twant/config/Config.java
+++ twant/app/src/main/java/com/ftofs/twant/config/Config.java
@@ -35,7 +35,7 @@ public class Config {
     public static  boolean PROD = false;//true不能進入調試頁面，false，可以進入調試頁面切換環境
     public static  boolean DEVELOPER_MODE = true;//線上模式：false,調試模式true
 
-    public static  boolean USE_28 = true;   // 開發模式下: true -- 使用28服務器  false -- 使用29服務器
+    public static  boolean USE_28 = false;   // 開發模式下: true -- 使用28服務器  false -- 使用29服務器
     public static final boolean USE_F2 = true;  // 生產模式下: true -- 使用F2服務器  false -- 使用www服務器
 
     /**
