23402557b7cae7b706d829dca651b88e8914fd68
guzhipeng1 <1281747107@qq.com>
2020-05-02 09:07:46 +0800

    重寫華爲推送服務

---
+++ twant/app/src/main/java/com/ftofs/twant/TwantHmsMessageService.java
@@ -0,0 +1,15 @@
+package com.ftofs.twant;
+
+import android.app.Service;
+
+import com.ftofs.twant.log.SLog;
+import com.huawei.hms.push.HmsMessageService;
+import com.huawei.hms.push.RemoteMessage;
+
+public class TwantHmsMessageService extends HmsMessageService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        SLog.info("TwantHmsMessageService::onMessageReceived");
+    }
+}
