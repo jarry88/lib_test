6afe6be01391b257e003ad731ec26495d0309e6b
guzhipeng1 <1281747107@qq.com>
2020-05-13 09:29:27 +0800

    退貨退款管理頁内容修改暫存

--- twant/app/src/main/java/com/ftofs/twant/fragment/SellerRefundFragment.java
+++ twant/app/src/main/java/com/ftofs/twant/fragment/SellerRefundFragment.java
@@ -1,6 +1,8 @@
 package com.ftofs.twant.fragment;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +15,8 @@ import com.ftofs.twant.R;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
-import me.yokeyword.fragmentation.ISupportFragment;
 
-public class SellerRefundFragment extends BaseFragment{
+public class SellerRefundFragment extends BaseFragment  {
 
     public static SellerRefundFragment newInstance() {
         SellerRefundFragment fragment = new SellerRefundFragment();
@@ -28,6 +29,7 @@ public class SellerRefundFragment extends BaseFragment{
     void back() {
         hideSoftInputPop();
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -37,16 +39,21 @@ public class SellerRefundFragment extends BaseFragment{
     }
 
     private Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_seller_refund_layout, container, false);
-        unbinder = ButterKnife.bind( this,view);
+        View view = inflater.inflate(R.layout.fragment_seller_return_info, container, false);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
+
+
+
 }
