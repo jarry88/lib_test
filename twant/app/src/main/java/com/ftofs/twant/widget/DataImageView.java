6f844e9c0a9934542526ed07ee00916f88671c8e
guzhipeng1 <1281747107@qq.com>
2020-05-13 14:18:52 +0800

    5094  Android - 【J】店鋪首頁的第三方社交平台外形優化

--- twant/app/src/main/java/com/ftofs/twant/widget/DataImageView.java
+++ twant/app/src/main/java/com/ftofs/twant/widget/DataImageView.java
@@ -1,16 +1,26 @@
 package com.ftofs.twant.widget;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * 可以設置自定義數據的ImageView
  * 為了應對setTag()操作時， 與Glide的衝突
  * @author zwm
  */
-public class DataImageView extends androidx.appcompat.widget.AppCompatImageView {
+public class DataImageView extends CircleImageView {
     Object customData;
+
     public DataImageView(Context context) {
         super(context);
     }
@@ -30,4 +40,5 @@ public class DataImageView extends androidx.appcompat.widget.AppCompatImageView 
     public void setCustomData(Object customData) {
         this.customData = customData;
     }
+
 }
