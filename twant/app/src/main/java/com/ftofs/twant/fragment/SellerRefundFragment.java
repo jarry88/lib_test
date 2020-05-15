6ab2e60ef1ebb24c8c82ab47dddab4d6e51dfdfd
guzhipeng1 <1281747107@qq.com>
2020-05-15 09:36:22 +0800

    暫存

--- twant/app/src/main/java/com/ftofs/twant/fragment/SellerRefundFragment.java
+++ twant/app/src/main/java/com/ftofs/twant/fragment/SellerRefundFragment.java
@@ -1,5 +1,6 @@
 package com.ftofs.twant.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -9,14 +10,29 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.ftofs.twant.R;
+import com.ftofs.twant.adapter.SimpleViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
 public class SellerRefundFragment extends BaseFragment  {
+    @BindView(R.id.vp_page_list)
+    ViewPager viewPager;
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
+    private List<View> mViews;
+    private PagerAdapter mPagerAdapter;
 
     public static SellerRefundFragment newInstance() {
         SellerRefundFragment fragment = new SellerRefundFragment();
@@ -51,9 +67,30 @@ public class SellerRefundFragment extends BaseFragment  {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initview(view);
 
     }
 
+    private void initview(View view) {
+        mViews = new ArrayList<>();
+        tabLayout.setupWithViewPager(viewPager);
+        TabLayout.Tab tabRefund = tabLayout.newTab();
+        TabLayout.Tab tabReTurn = tabLayout.newTab();
+        tabRefund.setCustomView(R.layout.tab_red_count_item);
+        tabReTurn.setCustomView(R.layout.tab_red_count_item);
+        tabRefund.setText(getResources().getString(R.string.text_refund_tab));
+        tabReTurn.setText(getResources().getString(R.string.text_refund_return_tab));
+        tabLayout.setTabTextColors(Color.parseColor("#2A292A"),Color.parseColor("#992A292A"));
+        tabLayout.addTab(tabRefund);
+        tabLayout.addTab(tabReTurn);
+        RecyclerView recyclerRefundView = new RecyclerView(_mActivity);
+        recyclerRefundView.setBackgroundColor(Color.BLUE);
+        RecyclerView recyclerReturnView = new RecyclerView(_mActivity);
+        mViews.add(recyclerRefundView);
+        mViews.add(recyclerReturnView);
+        mPagerAdapter = new SimpleViewPagerAdapter(mViews);
+        viewPager.setAdapter(mPagerAdapter);
+    }
 
 
 }
