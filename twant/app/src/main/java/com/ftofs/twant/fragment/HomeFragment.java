2f5317765f84498f0bfbee5c5c8b4e15a02665d1
zhouweiming <373518155@qq.com>
2020-06-05 20:12:09 +0800

    完成部分界面改造

--- twant/app/src/main/java/com/ftofs/twant/fragment/HomeFragment.java
+++ twant/app/src/main/java/com/ftofs/twant/fragment/HomeFragment.java
@@ -331,7 +331,7 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
 //            Util.startFragment(AddPostFragment.newInstance(false));
             ApiUtil.addPost(_mActivity,false);
         } else if (id == R.id.btn_test) {
-            Util.startFragment(AddGoodsFragment.newInstance());
+            Util.startFragment(LabFragment.newInstance());
 //            HwLoadingPopup loadingPopup = (HwLoadingPopup) new XPopup.Builder(_mActivity)
 //                    .dismissOnBackPressed(false) // 按返回键是否关闭弹窗，默认为true
 //                    .dismissOnTouchOutside(false) // 点击外部是否关闭弹窗，默认为true
