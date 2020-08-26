96abff431e36bf33c5226f2cb5e51ecd6d51143e
zhouweiming <373518155@qq.com>
2020-08-26 19:22:29 +0800

    禪道任務 5856 Android - 個人中心-安全設置功能優化

--- twant/app/src/main/java/com/ftofs/twant/fragment/SecuritySettingFragment.java
+++ twant/app/src/main/java/com/ftofs/twant/fragment/SecuritySettingFragment.java
@@ -27,12 +27,16 @@ import com.ftofs.twant.constant.Constant;
 import com.ftofs.twant.constant.EBMessageType;
 import com.ftofs.twant.constant.SPField;
 import com.ftofs.twant.entity.EBMessage;
+import com.ftofs.twant.interfaces.OnConfirmCallback;
 import com.ftofs.twant.log.SLog;
 import com.ftofs.twant.util.StringUtil;
 import com.ftofs.twant.util.ToastUtil;
 import com.ftofs.twant.util.User;
 import com.ftofs.twant.util.Util;
+import com.ftofs.twant.widget.TwConfirmPopup;
 import com.kyleduo.switchbutton.SwitchButton;
+import com.lxj.xpopup.XPopup;
+import com.lxj.xpopup.interfaces.XPopupCallback;
 import com.orhanobut.hawk.Hawk;
 
 import org.greenrobot.eventbus.EventBus;
@@ -56,9 +60,12 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
     int wxBindingStatus;
     int fbBindingStatus;
 
+    public static final int SNS_TYPE_WEIXIN = 1;
+    public static final int SNS_TYPE_FACEBOOK = 2;
 
-    private static final int FB_ACTION_BIND = 1;
-    private static final int FB_ACTION_UNBIND = 2;
+    public static final int ACTION_TYPE_BIND = 1;
+    public static final int ACTION_TYPE_UNBIND = 2;
+
     int fbAction;    // 用於區別Facebook綁定還是解綁
 
     CallbackManager callbackManager;
@@ -117,6 +124,7 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 SLog.info("onCheckedChanged, isChecked[%s]", isChecked);
                 if (isChecked) {
+                    SLog.info("綁定微信");
                     // 綁定微信
                     String token = User.getToken();
                     if (StringUtil.isEmpty(token)) {
@@ -125,8 +133,8 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
 
                     ((MainActivity) _mActivity).doWeixinLogin(Constant.WEIXIN_AUTH_USAGE_BIND);
                 } else {
-                    // 解綁微信
-                    ((MainActivity) _mActivity).doWeixinLogin(Constant.WEIXIN_AUTH_USAGE_UNBIND);
+                    SLog.info("解綁微信");
+                    showUnbindConfirm(SNS_TYPE_WEIXIN);
                 }
             }
         });
@@ -136,13 +144,16 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 SLog.info("onCheckedChanged, isChecked[%s]", isChecked);
                 if (isChecked) {
+                    SLog.info("綁定Facebook");
                     // 綁定Facebook
-                    fbAction = FB_ACTION_BIND;
+                    fbAction = ACTION_TYPE_BIND;
+                    LoginManager.getInstance().logInWithReadPermissions(SecuritySettingFragment.this, permissions);
                 } else {
+                    SLog.info("解綁Facebook");
                     // 解綁Facebook
-                    fbAction = FB_ACTION_UNBIND;
+                    fbAction = ACTION_TYPE_UNBIND;
+                    showUnbindConfirm(SNS_TYPE_FACEBOOK);
                 }
-                LoginManager.getInstance().logInWithReadPermissions(SecuritySettingFragment.this, permissions);
             }
         });
 
@@ -158,9 +169,11 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
                 String userId = accessToken.getUserId();
                 SLog.info("token[%s], userId[%s]", token, userId);
 
-                if (fbAction == FB_ACTION_BIND) {
+                if (fbAction == ACTION_TYPE_BIND) {
+                    SLog.info("綁定Facebook");
                     bindFacebook(token, userId);
                 } else {
+                    SLog.info("解綁Facebook");
                     unbindFacebook(token, userId);
                 }
             }
@@ -178,6 +191,60 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
                 SLog.info("onError, exception[%s]", exception.toString());
             }
         });
+    }
+
+    /**
+     * 顯示解綁的確認彈窗
+     * @param snsType
+     */
+    private void showUnbindConfirm(int snsType) {
+        String title = null;
+        String content = null;
+
+        if (snsType == SNS_TYPE_WEIXIN) {
+            title = "是否解除微信綁定";
+            content = "解除綁定後將無法使用微信登入";
+        } else if (snsType == SNS_TYPE_FACEBOOK) {
+            title = "是否解除Facebook綁定";
+            content = "解除綁定後將無法使用Facebook登入";
+        }
+
+        new XPopup.Builder(_mActivity)
+                .dismissOnBackPressed(false) // 按返回键是否关闭弹窗，默认为true
+                .dismissOnTouchOutside(false) // 点击外部是否关闭弹窗，默认为true
+                // 设置弹窗显示和隐藏的回调监听
+                // .autoDismiss(false)
+                .setPopupCallback(new XPopupCallback() {
+                    @Override
+                    public void onShow() {
+                    }
+                    @Override
+                    public void onDismiss() {
+                    }
+                }).asCustom(new TwConfirmPopup(_mActivity, title, content, "確認", "取消", new OnConfirmCallback() {
+            @Override
+            public void onYes() {
+                SLog.info("onYes");
+                if (snsType == SNS_TYPE_WEIXIN) {
+                    // 解綁微信
+                    ((MainActivity) _mActivity).doWeixinLogin(Constant.WEIXIN_AUTH_USAGE_UNBIND);
+                } else if (snsType == SNS_TYPE_FACEBOOK) {
+                    // 解綁Facebook
+                    LoginManager.getInstance().logInWithReadPermissions(SecuritySettingFragment.this, permissions);
+                }
+            }
+
+            @Override
+            public void onNo() {
+                SLog.info("onNo");
+                // 如果選No，恢復原先狀態
+                if (snsType == SNS_TYPE_WEIXIN) {
+                    sbBindWeixin.setCheckedNoEvent(true);
+                } else {
+                    sbBindFacebook.setCheckedNoEvent(true);
+                }
+            }
+        })).show();
     }
 
     private void bindFacebook(String accessToken, String userId) {
@@ -216,6 +283,11 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
         });
     }
 
+    /**
+     * 解綁Facebook
+     * @param accessToken facebook的token
+     * @param userId facebook的userId
+     */
     private void unbindFacebook(String accessToken, String userId) {
         String token = User.getToken();
         if (StringUtil.isEmpty(token)) {
@@ -320,82 +392,91 @@ public class SecuritySettingFragment extends BaseFragment implements View.OnClic
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEBMessage(EBMessage message) {
         if (message.messageType == EBMessageType.MESSAGE_TYPE_WEIXIN_UNBIND || message.messageType == EBMessageType.MESSAGE_TYPE_WEIXIN_BIND) {
-            String url;
+            int actionType;
             if (message.messageType == EBMessageType.MESSAGE_TYPE_WEIXIN_UNBIND) {
-                url = Api.PATH_UNBIND_WEIXIN;
+                actionType = ACTION_TYPE_UNBIND;
             } else {
-                url = Api.PATH_BIND_WEIXIN;
+                actionType = ACTION_TYPE_BIND;
             }
-
-            // 根據返回的數據，請求服務器端解綁微信
-            String token = User.getToken();
             String code = (String) message.data;
-            EasyJSONObject params = EasyJSONObject.generate(
-                    "token", token,
-                    "code", code,
-                    "clientType", Constant.CLIENT_TYPE_ANDROID
-            );
+            changeWeixinBinding(actionType, code);
+        }
+    }
 
-            SLog.info("url[%s], params[%s]", url, params);
-            Api.postUI(url, params, new UICallback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    ToastUtil.showNetworkError(_mActivity, e);
-                    SLog.info("message.messageType[%s]", message.messageType);
-                    // 失敗，則恢復回原狀態
-                    if (message.messageType == EBMessageType.MESSAGE_TYPE_WEIXIN_UNBIND) {
-                        sbBindWeixin.setCheckedNoEvent(true);
-                    } else {
-                        sbBindWeixin.setCheckedNoEvent(false);
-                    }
-                }
+    private void changeWeixinBinding(int actionType, String code) {
+        // 根據返回的數據，請求服務器端解綁微信
+        String token = User.getToken();
 
-                @Override
-                public void onResponse(Call call, String responseStr) throws IOException {
-                    try {
-                        SLog.info("responseStr[%s]", responseStr);
-                        EasyJSONObject responseObj = EasyJSONObject.parse(responseStr);
-
-                        if (ToastUtil.checkError(_mActivity, responseObj)) {
-                            // 失敗，則恢復回原狀態
-                            if (message.messageType == EBMessageType.MESSAGE_TYPE_WEIXIN_UNBIND) {
-                                sbBindWeixin.setCheckedNoEvent(true);
-                            } else {
-                                sbBindWeixin.setCheckedNoEvent(false);
-                            }
-                            return;
-                        }
-
-                        if (responseObj.exists("datas.bind")) {
-                            boolean bind = responseObj.getBoolean("datas.bind");
-                            if (!bind) {
-                                sbBindWeixin.setCheckedNoEvent(false);
-                                ToastUtil.success(_mActivity,responseObj.getSafeString("datas.message"));
-                                return;
-                            }
-                        }
-
-
-                        // 成功
-                        if (message.messageType == EBMessageType.MESSAGE_TYPE_WEIXIN_UNBIND) {
-                            //ToastUtil.success(_mActivity,responseObj.getString());
-                            sbBindWeixin.setCheckedNoEvent(false);
-                            ToastUtil.success(_mActivity,"解綁成功");
-                            wxBindingStatus = Constant.FALSE_INT;
-                        } else {
-                            sbBindWeixin.setCheckedNoEvent(true);
-                            ToastUtil.success(_mActivity,"綁定成功");
-                            wxBindingStatus = Constant.TRUE_INT;
-                        }
-
-                        Hawk.put(SPField.FIELD_WX_BINDING_STATUS, wxBindingStatus);
-                    } catch (Exception e) {
-
-                    }
-                }
-            });
+        String url;
+        if (actionType == ACTION_TYPE_UNBIND) {
+            url = Api.PATH_UNBIND_WEIXIN;
+        } else {
+            url = Api.PATH_BIND_WEIXIN;
         }
 
+        EasyJSONObject params = EasyJSONObject.generate(
+                "token", token,
+                "code", code,
+                "clientType", Constant.CLIENT_TYPE_ANDROID
+        );
+
+        SLog.info("url[%s], params[%s]", url, params);
+        Api.postUI(url, params, new UICallback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                ToastUtil.showNetworkError(_mActivity, e);
+                // 失敗，則恢復回原狀態
+                if (actionType == ACTION_TYPE_UNBIND) {
+                    sbBindWeixin.setCheckedNoEvent(true);
+                } else {
+                    sbBindWeixin.setCheckedNoEvent(false);
+                }
+            }
+
+            @Override
+            public void onResponse(Call call, String responseStr) throws IOException {
+                try {
+                    SLog.info("responseStr[%s]", responseStr);
+                    EasyJSONObject responseObj = EasyJSONObject.parse(responseStr);
+
+                    if (ToastUtil.checkError(_mActivity, responseObj)) {
+                        // 失敗，則恢復回原狀態
+                        if (actionType == ACTION_TYPE_UNBIND) {
+                            sbBindWeixin.setCheckedNoEvent(true);
+                        } else {
+                            sbBindWeixin.setCheckedNoEvent(false);
+                        }
+                        return;
+                    }
+
+                    if (responseObj.exists("datas.bind")) {
+                        boolean bind = responseObj.getBoolean("datas.bind");
+                        if (!bind) {
+                            sbBindWeixin.setCheckedNoEvent(false);
+                            ToastUtil.success(_mActivity,responseObj.getSafeString("datas.message"));
+                            return;
+                        }
+                    }
+
+
+                    // 成功
+                    if (actionType == ACTION_TYPE_UNBIND) {
+                        //ToastUtil.success(_mActivity,responseObj.getString());
+                        sbBindWeixin.setCheckedNoEvent(false);
+                        ToastUtil.success(_mActivity,"解綁成功");
+                        wxBindingStatus = Constant.FALSE_INT;
+                    } else {
+                        sbBindWeixin.setCheckedNoEvent(true);
+                        ToastUtil.success(_mActivity,"綁定成功");
+                        wxBindingStatus = Constant.TRUE_INT;
+                    }
+
+                    Hawk.put(SPField.FIELD_WX_BINDING_STATUS, wxBindingStatus);
+                } catch (Exception e) {
+                    SLog.info("Error!message[%s], trace[%s]", e.getMessage(), Log.getStackTraceString(e));
+                }
+            }
+        });
     }
 
     @Override
