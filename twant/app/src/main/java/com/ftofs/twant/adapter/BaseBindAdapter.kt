8bdd2154862b1413ab72af20c8278ebf221593bd
guzhipeng <1281747107@qq.com>
2020-08-28 16:20:15 +0800

    bindadapter 兼容child click 的寫法， 增加 setChildListRes 方法

--- twant/app/src/main/java/com/ftofs/twant/adapter/BaseBindAdapter.kt
+++ twant/app/src/main/java/com/ftofs/twant/adapter/BaseBindAdapter.kt
@@ -1,21 +1,31 @@
 package com.ftofs.twant.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
+import com.ftofs.twant.R
 import com.ftofs.twant.log.SLog
+import kotlinx.android.synthetic.main.sec_kill_list_normal_item.view.*
 
 abstract class BaseBindAdapter<T, D : ViewDataBinding>(val layoutResId: Int, data: List<T>) : BaseQuickAdapter<T, BoundViewHolder<D>>(layoutResId, data) {
+
+    private val childList by lazy { mutableListOf<Int>() }
+    fun setChildListRes(vararg int :Int){
+        int.forEach {childList.add(it)}
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):BoundViewHolder<D> {
         //        onCreateViewHolder(viewHolder,viewType);
         SLog.info(layoutResId.toString())
         mContext=parent.context
         mLayoutInflater= LayoutInflater.from(mContext)
-        val holder=BoundViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), layoutResId, parent, false) as D)
+        val holder=BoundViewHolder<D>(DataBindingUtil.inflate(LayoutInflater.from(parent.context), layoutResId, parent, false))
         when (viewType) {
             else->{
 //                bindViewClickListener(onCreateDefViewHolder(parent,viewType)
@@ -25,6 +35,7 @@ abstract class BaseBindAdapter<T, D : ViewDataBinding>(val layoutResId: Int, dat
 
         return holder
     }
+
 
     private fun bindClickListener(holder: BoundViewHolder<D>) {
         holder.let {
@@ -38,6 +49,9 @@ abstract class BaseBindAdapter<T, D : ViewDataBinding>(val layoutResId: Int, dat
                     setOnItemClick(it,position)
                 }
             }
+        }
+        childList?.forEach{
+            holder.binding.root.findViewById<View>(it).setOnClickListener { onItemChildClickListener?.onItemChildClick(null,it,holder.absoluteAdapterPosition) }
         }
     }
 
--- twant/app/src/main/java/com/ftofs/twant/adapter/BoundViewHolder.kt
+++ twant/app/src/main/java/com/ftofs/twant/adapter/BoundViewHolder.kt
@@ -1,7 +1,34 @@
 package com.ftofs.twant.adapter
 
+import android.view.View
+import android.widget.ImageView
 import androidx.databinding.ViewDataBinding
 import com.chad.library.adapter.base.BaseViewHolder
+import com.ftofs.twant.R
+import com.ftofs.twant.log.SLog
+import kotlinx.android.synthetic.main.image_popup.view.*
 
-class BoundViewHolder<out T : ViewDataBinding> constructor(val binding: T) :
+class BoundViewHolder< out T : ViewDataBinding> constructor(val binding: T) :
        BaseViewHolder(binding.root)
+
+
+//    fun addClickListener(vararg viewIds: Int): BoundViewHolder {
+////        return super.addOnClickListener(*viewIds)
+//        viewIds.forEach {
+//            childClickViewIds.add(it)
+//            getView<View>(it)?.apply{
+//                if(!isClickable)isClickable=true
+//                setOnClickListener {
+//                    mAdapter.apply {
+//                        var position=absoluteAdapterPosition
+//                        if(position== RecyclerView.NO_POSITION){
+//                            return@setOnClickListener
+//                        }
+//                        position -=headerLayoutCount
+//                        onItemChildClickListener?.onItemChildClick(this,it,position)
+//                    }
+//                }
+//            }
+//        }
+//        return this
+//    }
