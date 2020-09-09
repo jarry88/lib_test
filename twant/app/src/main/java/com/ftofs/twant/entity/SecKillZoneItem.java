5809460922f242795bc0fda13d12d6dae8d8b5d1
zhouweiming <373518155@qq.com>
2020-09-09 12:09:45 +0800

    修復BUG 1613 Android，秒殺專區，未開始場次修改為：開始時間倒計時

--- twant/app/src/main/java/com/ftofs/twant/entity/SecKillZoneItem.java
+++ twant/app/src/main/java/com/ftofs/twant/entity/SecKillZoneItem.java
@@ -6,4 +6,5 @@ public class SecKillZoneItem {
     public int scheduleId;
     public String startTime;
     public String statusText;
+    public int scheduleState;  // 状态 0-即將開場 1-已開搶 2-已結束 10-等待審核 20-審核失敗
 }
